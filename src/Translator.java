2f2bc287b4765f4d3624b2529bb34dc50867e890
Guillaume Aime A Bogaert <guillaumebogaert244@gmail.com>
2026-09-01 13:09:06 -0400

    work on translator

--- src/Translator.java
+++ src/Translator.java
@@ -1,5 +1,17 @@
-public class Translator {
-// pull request check
-// another check
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class Translator {
+    File file = new File ("Example.java");
+
+public void readFile() throws FileNotFoundException {
+    Scanner scanner = new Scanner (file);
+    while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        System.out.println(line);
+        
+    }
+
+}
 }
